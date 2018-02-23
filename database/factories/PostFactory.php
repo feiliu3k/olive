662f8f89233d278db0762bc710a7fab7201bd022
feiliu3k <1104829018@qq.com>
2018-02-23 09:47:49 +0000

    add post

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'user_id' => factory(\App\User::class)->create()->id
     ];
 });
